be92e4d35060e491b293dfcedac8f7b35ec4a97e
emmoistner <emmoistner@gmail.com>
2013-10-27 07:41:26 -0400

    v .02
    Worksheet_Change
    More Dynamic Variables
    Rearranged If States
    Rid of while break

--- AppsValidation.vb
+++ AppsValidation.vb
@@ -1,39 +1,50 @@
-Sub Button1_Click()
+Private Sub Worksheet_Change(ByVal Target As Range)
 Dim Max As Integer
 Dim Counter As Integer
 Dim RowValue As Integer
 Dim CategoryContents As String
 Dim RootName As String
+Dim Roots(15) As Integer
+Dim RootNameRow As String
+Dim CategoryRow As String
+Dim WarningColor As Long
+Dim ErrorColor As Long
+WarningColor = RGB(255, 255, 55)
+ErrorColor = RGB(255, 41, 41)
+CategoryRow = "C"
+RootNameRow = "A"
 Max = 5
 Counter = 0
 RowValue = 3
 Do While RowValue < 10
-    CategoryContents = Cells(RowValue, "C").Value
+    CategoryContents = Cells(RowValue, CategoryRow).Value
     If CategoryContents = "ROOT" Then
         Counter = (Counter + 1)
+        Roots(Counter) = RowValue
     ElseIf LCase(CategoryContents) = "root" Then
         Counter = (Counter + 1)
-        Cells(RowValue, "C").Value = "ROOT"
+        Cells(RowValue, CategoryRow).Value = "ROOT"
+        Roots(Counter) = RowValue
     End If
-    RootName = Cells(RowValue, "A").Text
-    If Len(RootName) > 11 Then
-        Cells(RowValue, "A").Interior.Color = RGB(255, 41, 41)
-    Else
-        Cells(RowValue, "A").Interior.ColorIndex = 0
-    End If
-    
+    RootName = Cells(RowValue, RootNameRow).Text
     If Len(RootName) > 0 And Len(CategoryContents) < 1 Then
-        Cells(RowValue, "C").Interior.Color = RGB(255, 41, 41)
+        Cells(RowValue, CategoryRow).Interior.Color = ErrorColor
     ElseIf Len(RootName) < 1 And Len(CategoryContents) > 0 Then
-        Cells(RowValue, "A").Interior.Color = RGB(255, 41, 41)
+        Cells(RowValue, RootNameRow).Interior.Color = ErrorColor
     Else
-    Cells(RowValue, "A").Interior.ColorIndex = 0
-    Cells(RowValue, "C").Interior.ColorIndex = 0
+         If Len(RootName) > 11 Then
+            Cells(RowValue, RootNameRow).Interior.Color = ErrorColor
+        Else
+            Cells(RowValue, RootNameRow).Interior.ColorIndex = 0
+            Cells(RowValue, CategoryRow).Interior.ColorIndex = 0
+        End If
     End If
     RowValue = (RowValue + 1)
-    If Counter > Max Then
-        MsgBox ("Too Many Roots")
-        Exit Do
-    End If
 Loop
+If Counter > Max Then
+    
+End If
 End Sub
+
+
+
