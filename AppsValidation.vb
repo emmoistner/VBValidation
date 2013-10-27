04852436f588f7b56765c5dfaad7cbfec61eef9e
emmoistner <emmoistner@gmail.com>
2013-10-27 09:41:15 -0400

    v .03
    Warning on excessive ROOTS.

--- AppsValidation.vb
+++ AppsValidation.vb
@@ -4,11 +4,12 @@ Dim Counter As Integer
 Dim RowValue As Integer
 Dim CategoryContents As String
 Dim RootName As String
-Dim Roots(15) As Integer
 Dim RootNameRow As String
 Dim CategoryRow As String
 Dim WarningColor As Long
 Dim ErrorColor As Long
+Dim AdjustedCounter As Integer
+Dim RootThreshold As Integer
 WarningColor = RGB(255, 255, 55)
 ErrorColor = RGB(255, 41, 41)
 CategoryRow = "C"
@@ -16,15 +17,14 @@ RootNameRow = "A"
 Max = 5
 Counter = 0
 RowValue = 3
-Do While RowValue < 10
+RootThreshold = 10
+Do While RowValue < RootThreshold
     CategoryContents = Cells(RowValue, CategoryRow).Value
     If CategoryContents = "ROOT" Then
         Counter = (Counter + 1)
-        Roots(Counter) = RowValue
     ElseIf LCase(CategoryContents) = "root" Then
         Counter = (Counter + 1)
         Cells(RowValue, CategoryRow).Value = "ROOT"
-        Roots(Counter) = RowValue
     End If
     RootName = Cells(RowValue, RootNameRow).Text
     If Len(RootName) > 0 And Len(CategoryContents) < 1 Then
@@ -41,8 +41,22 @@ Do While RowValue < 10
     End If
     RowValue = (RowValue + 1)
 Loop
+AdjustedCounter = Counter
+RowValue = 3
 If Counter > Max Then
-    
+    Do While RowValue < RootThreshold
+    	CategoryContents = Cells(RowValue, CategoryRow).Value
+    	If Cells(RowValue, CategoryRow).Interior.Color = ErrorColor Then
+    	    RowValue = RowValue + 1
+    	End If
+    	CategoryContents = Cells(RowValue, CategoryRow).Value
+    	If CategoryContents = "ROOT" Then
+            Cells(RowValue, CategoryRow).Interior.Color = WarningColor
+    	    RowValue = RowValue + 1
+    	Else
+    	    RowValue = RowValue + 1
+        End If
+    Loop
 End If
 End Sub
 
