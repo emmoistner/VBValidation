0e471b562f082718d5ad894da8a5e54251813840
emmoistner <emmoistner@gmail.com>
2013-10-30 18:45:59 -0400

    V.031
    Reworked Thresholds
    Added Trim to cell values (Validation!)
    Fixed TOO MANY ROOT detection. Functionally sound

--- AppsValidation.vb
+++ AppsValidation.vb
@@ -1,7 +1,6 @@
 Private Sub Worksheet_Change(ByVal Target As Range)
 Dim Max As Integer
-Dim Counter As Integer
-Dim RowValue As Integer
+Dim RowCounter As Integer
 Dim CategoryContents As String
 Dim RootName As String
 Dim RootNameRow As String
@@ -10,55 +9,61 @@ Dim WarningColor As Long
 Dim ErrorColor As Long
 Dim AdjustedCounter As Integer
 Dim RootThreshold As Integer
+Dim Counter As Integer
+Dim StartRow As Integer
 WarningColor = RGB(255, 255, 55)
 ErrorColor = RGB(255, 41, 41)
 CategoryRow = "C"
 RootNameRow = "A"
 Max = 5
 Counter = 0
-RowValue = 3
+StartRow = 3
 RootThreshold = 10
-Do While RowValue < RootThreshold
-    CategoryContents = Cells(RowValue, CategoryRow).Value
+RowCounter = StartRow
+Do While RowCounter < RootThreshold
+    CategoryContents = Cells(RowCounter, CategoryRow).Text
+    CategoryContents = Trim(CategoryContents)
+    RootName = Cells(RowCounter, RootNameRow).Text
+    RootName = Trim(RootName)
     If CategoryContents = "ROOT" Then
         Counter = (Counter + 1)
     ElseIf LCase(CategoryContents) = "root" Then
         Counter = (Counter + 1)
-        Cells(RowValue, CategoryRow).Value = "ROOT"
+        Cells(RowCounter, CategoryRow).Value = "ROOT"
     End If
-    RootName = Cells(RowValue, RootNameRow).Text
     If Len(RootName) > 0 And Len(CategoryContents) < 1 Then
-        Cells(RowValue, CategoryRow).Interior.Color = ErrorColor
+        Cells(RowCounter, CategoryRow).Interior.Color = ErrorColor
     ElseIf Len(RootName) < 1 And Len(CategoryContents) > 0 Then
-        Cells(RowValue, RootNameRow).Interior.Color = ErrorColor
+        Cells(RowCounter, RootNameRow).Interior.Color = ErrorColor
+    ElseIf Len(RootName) > 11 And CategoryContents = "ROOT" Then
+        Cells(RowCounter, RootNameRow).Interior.Color = ErrorColor
     Else
-         If Len(RootName) > 11 Then
-            Cells(RowValue, RootNameRow).Interior.Color = ErrorColor
-        Else
-            Cells(RowValue, RootNameRow).Interior.ColorIndex = 0
-            Cells(RowValue, CategoryRow).Interior.ColorIndex = 0
-        End If
+        Cells(RowCounter, RootNameRow).Interior.ColorIndex = 0
+        Cells(RowCounter, CategoryRow).Interior.ColorIndex = 0
     End If
-    RowValue = (RowValue + 1)
+    RowCounter = (RowCounter + 1)
 Loop
 AdjustedCounter = Counter
-RowValue = 3
+RowCounter = StartRow
 If Counter > Max Then
-    Do While RowValue < RootThreshold
-    	CategoryContents = Cells(RowValue, CategoryRow).Value
-    	If Cells(RowValue, CategoryRow).Interior.Color = ErrorColor Then
-    	    RowValue = RowValue + 1
-    	End If
-    	CategoryContents = Cells(RowValue, CategoryRow).Value
-    	If CategoryContents = "ROOT" Then
-            Cells(RowValue, CategoryRow).Interior.Color = WarningColor
-    	    RowValue = RowValue + 1
-    	Else
-    	    RowValue = RowValue + 1
+    Do While RowCounter < RootThreshold
+        
+        CategoryContents = Cells(RowCounter, CategoryRow).Value
+        CategoryContents = Trim(CategoryContents)
+        If Cells(RowCounter, CategoryRow).Interior.Color = ErrorColor Then
+            RowCounter = RowCounter + 1
+        Else
+            If CategoryContents = "ROOT" Then
+                Cells(RowCounter, CategoryRow).Interior.Color = WarningColor
+                RowCounter = RowCounter + 1
+            Else
+                RowCounter = RowCounter + 1
+            End If
         End If
     Loop
 End If
 End Sub
+
 
 
 
